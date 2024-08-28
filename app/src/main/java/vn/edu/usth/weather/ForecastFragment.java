efdf061197936dbf5cca5477ead940fd1883011a
hatrungnguyen <hatrungnguyen04@gmail.com>
2024-08-28 15:59:04 +0700

    change to java and pra3 and pra4

---
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -0,0 +1,55 @@
+package vn.edu.usth.weather;
+
+import android.os.Bundle;
+import android.graphics.Color;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class ForecastFragment extends Fragment {
+
+    public ForecastFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        LinearLayout linearLayout = new LinearLayout(getContext());
+        linearLayout.setOrientation(LinearLayout.VERTICAL);
+        linearLayout.setLayoutParams(new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT
+        ));
+        linearLayout.setBackgroundColor(Color.parseColor("#200000FF"));
+        TextView textView = new TextView(getContext());
+        textView.setText(" A day ");
+        textView.setTextColor(Color.MAGENTA);
+        textView.setTextSize(24);
+        LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        linearLayout.addView(textView, textParams);
+        ImageView imageView = new ImageView(getContext());
+        imageView.setImageResource(R.drawable.cloudy);
+        LinearLayout.LayoutParams imageParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+        );
+        linearLayout.addView(imageView, imageParams);
+
+        return linearLayout;
+    }
+
+}
